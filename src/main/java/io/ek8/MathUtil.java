ea913f4e4d5beda02bf7348785fc40c3a3d225fc
E-K8 <ekatplat@gmail.com>
2023-12-09 23:22:44 +0000

    set up and first test shell

---
+++ src/main/java/io/ek8/MathUtil.java
@@ -0,0 +1,7 @@
+package io.ek8;
+
+public class MathUtil {
+  public int add(int x, int y) {
+    return x + y;
+  }
+}
---
+++ src/test/java/io/ek8/MathUtilTest.java
@@ -0,0 +1,9 @@
+package io.ek8;
+
+import static org.junit.jupiter.api.Assertions.*;
+class MathUtilTest {
+
+  @org.junit.jupiter.api.Test
+  void add() {
+  }
+}
