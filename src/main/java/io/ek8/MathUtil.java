2bb4a672825e6ee5fe716027a11c1df46eafb0fd
E-K8 <ekatplat@gmail.com>
2023-12-10 21:39:17 +0000

    assertEquals method

--- src/main/java/io/ek8/MathUtil.java
+++ src/main/java/io/ek8/MathUtil.java
@@ -4,4 +4,7 @@ public class MathUtil {
   public int add(int x, int y) {
     return x + y;
   }
+  public int subtract(int x, int y) {
+    return x - y;
+  }
 }
--- src/test/java/io/ek8/MathUtilTest.java
+++ src/test/java/io/ek8/MathUtilTest.java
@@ -1,9 +1,30 @@
 package io.ek8;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MathUtilTest {
 
-  @org.junit.jupiter.api.Test
+  @Test
   void add() {
+//    given
+    int x = 2, y = 1;
+//    when
+    MathUtil mathUtil = new MathUtil();
+    int actual = mathUtil.add(x, y);
+//    then
+    assertEquals(3, actual);
+  }
+
+  @Test
+  void subtract() {
+    //    given
+    int x = 11, y = 2;
+//    when
+    MathUtil mathUtil = new MathUtil();
+    int actual = mathUtil.subtract(x, y);
+//    then
+    assertEquals(9, actual);
   }
 }
